07b3bbaf190998af15617b3c74362450e233dc8a
Jose V Sebastian <josevseb99@gmail.com>
2023-10-01 02:03:36 +0530

    ci: Adds semantic release

---
+++ release.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import('semantic-release').GlobalConfig} */
+const config = {
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    "next",
+    "next-major",
+    { name: "beta", prerelease: "beta" },
+    { name: "alpha", prerelease: "alpha" },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["dist/**/*.{js,js.map,d.ts,d.ts.map}", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
+
+module.exports = config;
